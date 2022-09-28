f8001a192b08c7fa5af1f6ffd08847de99d46877
rafafcorreia <rafa.f.correia@gmail.com>
2022-09-28 20:17:28 -0300

    Primeiro commit

---
+++ readme.md
@@ -0,0 +1,14 @@
+# PetstoreJS
+
+## Instruções
+
+1. Instalar as bibliotecas: `npm install supertest mocha chai --save-dev`
+
+2. Certifique-se que o seguinte bloco de código está no seu package.json:
+`
+"scripts": {
+    "test": "jest"
+  }
+`
+
+3. Rodar os testes: `npm test`
---
+++ test/api/pet.spec.js
@@ -0,0 +1,51 @@
+const assert = require('chai').assert;
+
+const supertest = require('supertest');
+
+const petID = 123129;
+
+describe('Petstore Swagger', () => {
+
+    const request = supertest('https://petstore.swagger.io/v2');
+
+    it('POST Pet', () => {
+        const pet = require('../../vendors/pet.json');
+        return request
+            .post('/pet')
+            .send(pet) // send payload data
+            .then((res) => {
+                assert.equal(res.statusCode, 200);
+                assert.equal(res.body.id, petID);
+                assert.equal(res.body.name, "Dog")
+            });
+    });
+
+    it('GET Pet', () => {
+        return request
+            .get('/pet/' + petID)
+            .then((res) => {
+                assert.equal(res.statusCode, 200);
+                assert.equal(res.body.id, petID);
+            })
+    })
+
+    it('PUT Pet', () => {
+        const petNovo = require('../../vendors/petNovo.json');
+        return request
+            .put('/pet')
+            .send(petNovo)
+            .then((res) => {
+                assert.equal(res.statusCode, 200);
+                assert.equal(res.body.id, petID);
+                assert.equal(res.body.name, "Athena");
+            })
+    })
+
+    it('DELETE Pet', () => {
+        return request
+            .delete('/pet/' + petID)
+            .then((res) => {
+                assert.equal(res.statusCode, 200);
+            })
+    })
+});

d18b6092c4cfc074094d107740c76121fbd3deb9
rafafcorreia <rafa.f.correia@gmail.com>
2022-11-03 18:22:48 -0300

    JSON montado dinamicamente

--- test/api/pet.spec.js
+++ test/api/pet.spec.js
@@ -7,32 +7,63 @@ const petID = 123129;
 describe('Petstore Swagger - Pet', () => {
 
     const request = supertest('https://petstore.swagger.io/v2');
+    const massa = require('../../vendors/json/massaPet')
+    const pet = require('../../vendors/json/pet.json');
 
-    it('POST Pet', () => {
-        const pet = require('../../vendors/pet.json');
-        return request
-            .post('/pet')
-            .send(pet) // send payload data
-            .then((res) => {
-                assert.equal(res.statusCode, 200);
-                assert.equal(res.body.id, petID);
-                assert.equal(res.body.name, "Dog")
-                assert.equal(res.body.photoUrls[0], "string");
-                assert.equal(res.body.category.id, 0);
-                assert.equal(res.body.tags[0].name, "string")
-            });
+    /*it.only.each(massa.array.map(elemento => [
+        elemento.nomePet,
+        elemento.idPet,
+        elemento.nomeCategoria,
+        elemento.idCategoria]))
+        ('POST Pet %s', (nomePet, idPet, nomeCategoria, idCategoria) => {
+
+            pet.id = idPet
+            pet.name = nomePet
+            pet.category.id = idCategoria
+            pet.category.name = nomeCategoria */
+    massa.array.forEach(({ nomePet, idPet, nomeCategoria, idCategoria }) => {
+
+        it(`POST Pet - ${nomePet}`, () => {
+
+            pet.id = idPet
+            pet.name = nomePet
+            pet.category.id = idCategoria
+            pet.category.name = nomeCategoria
+
+            return request
+                .post('/pet')
+                .send(pet) // send payload data
+                .then((res) => {
+                    assert.equal(res.statusCode, 200);
+                    assert.equal(res.body.id, idPet);
+                    assert.equal(res.body.name, nomePet)
+                    assert.equal(res.body.category.id, idCategoria);
+                    assert.equal(res.body.category.name, nomeCategoria)
+                });
+        })
+
+        it(`GET Pet - ${nomePet}`, () => {
+            return request
+                .get('/pet/' + idPet)
+                .then((res) => {
+                    assert.equal(res.statusCode, 200);
+                    assert.equal(res.body.id, idPet);
+                })
+        })
+
+        it(`DELETE Pet - ${nomePet}`, () => {
+            return request
+                .delete('/pet/' + idPet)
+                .then((res) => {
+                    assert.equal(res.statusCode, 200);
+                })
+        })
+
     });
 
-    it('GET Pet', () => {
-        return request
-            .get('/pet/' + petID)
-            .then((res) => {
-                assert.equal(res.statusCode, 200);
-                assert.equal(res.body.id, petID);
-            })
-    })
 
-    it('PUT Pet', () => {
+
+    it.skip('PUT Pet', () => {
         const petNovo = require('../../vendors/petNovo.json');
         return request
             .put('/pet')
@@ -44,11 +75,4 @@ describe('Petstore Swagger - Pet', () => {
             })
     })
 
-    it('DELETE Pet', () => {
-        return request
-            .delete('/pet/' + petID)
-            .then((res) => {
-                assert.equal(res.statusCode, 200);
-            })
-    })
 });
---
+++ vendors/json/massaPet.js
@@ -0,0 +1,20 @@
+module.exports.array = [
+    {
+        "nomePet": "Bolt",
+        "idPet": 123129,
+        "nomeCategoria": "Dog",
+        "idCategoria": 0
+    },
+    {
+        "nomePet": "Spike",
+        "idPet": 129123,
+        "nomeCategoria": "Dog",
+        "idCategoria": 0
+    },
+    {
+        "nomePet": "Lina",
+        "idPet": 890098,
+        "nomeCategoria": "Cat",
+        "idCategoria": 1
+    }
+]
